87e98828d9f50088b88e13bf29e565402efd4147
wro2 <32654515+wro2@users.noreply.github.com>
2022-08-23 19:14:47 -0400

    Added Year Folders & Recursive Indexing
    Added ability to build file list recursively from folders. Added the ability to create destination folders by year automatically.

--- main.py
+++ main.py
@@ -3,110 +3,77 @@ import os
 import shutil
 from exif import Image
 from datetime import datetime
+import random
+import dateutil.parser
 
 
 #TODO: Figure out subdirectories. Throws an error when processing subdirectory (in with-open)
-#TODO: Retain file extension.
-#TODO: Figure out .gif files.
 #TODO: Add Logging
-#TODO: Sort by Year
-#TODO: Genreal cleanup and creation of functions.
+#TODO: Add Config Options
+#TODO: Gen cleanup and creation of functions.
 
+ #Create a list of files and subdirectories in the given path.
+ 
+def get_file_list(dirName):
+    allFiles = list()   
+    for dir_, _, files in os.walk(dirName):
+         for file_name in files:           
+            rel_file = os.path.join(dir_, file_name)
+            allFiles.append(rel_file)
 
-def getListOfFiles(dirName):
-  #Create a list of files and subdirectories in the given path.
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-                
     return allFiles
+        
 
-  
-file_list = getListOfFiles("./testdata/")
-for file in file_list:    
+
+def move_to_error_directory(file):
+    file_name_parts = os.path.splitext(file)    
+    full_dest_path = "./error/" + os.path.basename(file)
+    shutil.copy(file,full_dest_path)
+
+#Extracts exif date-time from file
+def get_exif_data(file):      
     with open(file,"rb") as image_file:
         current_image = Image(image_file)   
 
+    #Set Default date if no exif data exists for date taken.
     date_taken = "1977:01:01 00:00:01"
     if current_image.has_exif:       
         exif_tags = dir(current_image)
         for tag in exif_tags:
             if tag == "datetime_original":
                 date_taken = current_image["datetime_original"]           
-
-    date_time_obj = datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S') 
-
-   
-    full_dest_path = "./processed/" + + date_time_obj.strftime("%Y-%m-%d_%H%M%S") + ".jpg"
-    shutil.copy(file,full_dest_path)
-  
-
         
-
-   
     
-    
+    #TODO: Need to write more verbose code to handle custom date formats
+    try:
+            date_time_obj = dateutil.parser.parse(date_taken)
+    except:
+            date_time_obj = dateutil.parser.parse("1977:01:01 00:00:01")
+    return date_time_obj  
 
+def rename_and_move(file,date_taken):
+    file_year = date_taken.strftime("%Y")   
+    if not os.path.exists("./processed/"+file_year):        
+         os.makedirs("./processed/"+file_year)  
 
-
-
-
-
-#TODO: Figure out subdirectories. Throws an error when processing subdirectory for GetLIstofFiles
-#TODO: Retain file extension.
-#TODO: Figure out .gif files.
-#TODO: Add Logging
-#TODO: Add folder by Year
-
-def getListOfFiles(dirName):
-  #Create a list of files and subdirectories in the given path.
-    listOfFile = os.listdir(dirName)
-    allFiles = list()
-    # Iterate over all the entries
-    for entry in listOfFile:
-        # Create full path
-        fullPath = os.path.join(dirName, entry)
-        # If entry is a directory then get the list of files in this directory 
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-                
-    return allFiles
-
-  
-file_list = getListOfFiles("./testdata/")
-for file in file_list:    
-    with open(file,"rb") as image_file:
-        current_image = Image(image_file)   
-
-    date_taken = "1977:01:01 00:00:01"
-    if current_image.has_exif:       
-        exif_tags = dir(current_image)
-        for tag in exif_tags:
-            if tag == "datetime_original":
-                date_taken = current_image["datetime_original"]           
-
-    date_time_obj = datetime.strptime(date_taken, '%Y:%m:%d %H:%M:%S') 
-
-   
-    full_dest_path = "./processed/" +  date_time_obj.strftime("%Y-%m-%d_%H%M%S") + ".jpg"
+    file_name_parts = os.path.splitext(file)
+    full_dest_path = "./processed/" + file_year + "/" +  str(date_taken.strftime("%Y-%m-%d")) + "_" + str(random.randint(1,100000)) + file_name_parts[1]
     shutil.copy(file,full_dest_path)
+
+
+
+#-------------------------  
+#Main Execution
+#-------------------------
+file_list = get_file_list("./testdata/")
+for file in file_list:
+  #  print ("processing:  " + file)
+    if file.endswith("gif") or file.endswith("psd") or file.endswith("xcf") :
+        move_to_error_directory(file)
+    else:
+        date_taken = get_exif_data(file)
+        rename_and_move(file,date_taken)
+    
+
+
   
-
-        
-
-   
-    
-    
-
-
-
